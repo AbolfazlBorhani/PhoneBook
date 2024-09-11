23a328727b639950ba4be46dc59b392d3f547b88
ABOLFAZL <abolfazlborhani80@gmail.com>
2024-09-11 21:00:38 +0330

    Update PhoneBook.pro to include new features.

---
+++ C++/main.cpp
@@ -0,0 +1,15 @@
+#include "phonebook.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+
+    PhoneBook w{};
+
+    w.readContactsFromFile();
+    w.setIconAndTitle(w);
+    w.show();
+
+    return a.exec();
+}
